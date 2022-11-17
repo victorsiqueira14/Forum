02a5b1c5a8ee89bc0d5f31e69ab03e7b5e7cf481
Victor <victorms.adm@gmail.com>
2022-11-17 09:41:57 -0300

    Criando as seeders, factorys, tables

---
+++ database/factories/ThreadFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class ThreadFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $userIds = User::all()->pluck('id');
+
+        return [
+           'user_id' => fake()->randomElement($userIds),
+           'title' => fake()->sentence,
+           'body' => fake()->paragraph,
+        ];
+    }
+}
+      
