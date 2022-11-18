6334add058146c5912515ba3d6ce395cc880bb2d
Victor <victorms.adm@gmail.com>
2022-11-17 23:15:31 -0300

    Thread And replies

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Reply extends Model
 {
@@ -14,4 +16,13 @@ class Reply extends Model
         'thread_id',
         'body',
     ];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+
+
 }
+
---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Reply;
+use App\Models\Thread;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ReadThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    private $thread;
+    private $user;
+
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+        $this->thread = Thread::factory()->create();
+
+    }
+
+    public function test_a_user_can_browse_threads()
+    {
+
+        $this
+        ->get('threads')
+        ->assertSee($this->thread->title);
+    }
+
+    public function test_a_user_can_read_a_single_thread()
+    {
+            $this->get('/threads/'. $this->thread->id)
+            ->assertSee($this->thread->title);
+
+    }
+
+    public function test_a_user_can_replies_that_are_associated_with_a_thread()
+    {
+        // given we have a thread that
+        // and that thread includes replies
+        $reply = Reply::factory()
+            ->create(['thread_id' => $this->thread->id]);
+        //when we visit a thread page
+        //then we should see that replies
+        $this->get('/threads/'. $this->thread->id)
+            ->assertSee($reply->body);
+    }
+
+
+
+}
+
+
+
+
