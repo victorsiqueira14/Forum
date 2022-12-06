bb81f322a6c65d6deb110c43e6fa6d05c628bb16
Victor <victorms.adm@gmail.com>
2022-12-06 13:12:34 -0300

    a user can Delete their threads

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -5,24 +5,22 @@
         <div class="row  justify-content-center">
             <div class="col-md-8">
                 <div class="">
-                    <div class="card">
-                        <div class="card-header "> Forum Threads</div>
-                    </div>
-
+                    {{-- titulo ??  se for coloque aqui --}}
                     @foreach ($threads as $thread)
                         <div class="mt-2">
                             <article class="card">
                                 <div class="card-header">
 
                                     <div class="level">
-                                    <h4 class="flex">
-                                        <a class="" href="/threads/{{ $thread->path() }}">
-                                            {{ $thread->title }}
-                                        </a>
-                                    </h4>
+                                        <h4 class="flex">
+                                            <a class="" href="/threads/{{ $thread->path() }}">
+                                                {{ $thread->title }}
+                                            </a>
+                                        </h4>
 
-                                    <a href="/threads/{{ $thread->path() }}">
-                                        {{ $thread->replies_count }} {{ Str::plural('reply', $thread->replies_count) }}</a>
+                                        <a href="/threads/{{ $thread->path() }}">
+                                            {{ $thread->replies_count }}
+                                            {{ Str::plural('reply', $thread->replies_count) }}</a>
                                     </div>
 
 
